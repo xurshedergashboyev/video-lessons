0dd87590b3a3540055bfaa01ca08031a53ccca91
xurshedergashboyev <xurshedergashboyev@gmail.com>
2021-09-16 11:12:43 +0500

    final changes

--- src/contants/chatId.js
+++ src/contants/chatId.js
@@ -1,4 +1,4 @@
 export const mainChatId = '513214213'
 export const secondaryChatId = '463859749'
 
-// export const testChatId = '286602133' // maraim
+export const testChatId = '286602133' // maraim
